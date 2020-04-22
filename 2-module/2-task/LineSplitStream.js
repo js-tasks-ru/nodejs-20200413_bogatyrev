6f2668329e36b028bca4438a3514b07846907417
Anton Bogatyrev <ab.developer@yandex.ru>
2020-04-22 14:15:52 +0300

    Add solution for 2-module 2-task.

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,40 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.incompleteLine = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const chunkStr = chunk.toString();
+    const lines = chunkStr.split(os.EOL);
+    const currentIncompleteLine = lines.pop();
+
+    for (let line of lines) {
+      if (this.incompleteLine) {
+        line = this.incompleteLine + line;
+        this.incompleteLine = '';
+      }
+
+      this.push(line);
+    }
+
+    // save incomplete line in memory
+    if (this.incompleteLine) {
+      this.incompleteLine += currentIncompleteLine;
+    } else {
+      this.incompleteLine = currentIncompleteLine;
+    }
+
+    callback(null);
   }
 
   _flush(callback) {
+    if (this.incompleteLine) {
+      this.push(this.incompleteLine);
+      this.incompleteLine = '';
+    }
+
+    callback(null);
   }
 }
 

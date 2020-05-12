462ff82a2b694b8191e4d4f219f21a48d0252386
Anton Bogatyrev <ab.developer@yandex.ru>
2020-05-12 18:40:16 +0300

    Added solution for 9-module 1-task.

--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -7,11 +7,34 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+    const token = socket.handshake.query.token;
+    if (!token) {
+      return next(new Error("anonymous sessions are not allowed"));
+    }
+
+    const session = await Session.findOne({token: token}).populate('user');
+    if (!session) {
+      return next(new Error("wrong or expired session token"));
+    }
+
+    socket.user = session.user;
     next();
   });
 
-  io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+  io.on('connection', function(socket, next) {
+
+    socket.on('message', async (msg) => {
+      try {
+        await Message.create({
+          user: socket.user.displayName,
+          chat: socket.user.id,
+          text: msg,
+          date: new Date(),
+        });
+      } catch (e) {
+        next(e.errors.shift());
+      }
+    });
   });
 
   return io;
